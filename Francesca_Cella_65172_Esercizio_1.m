5c8073b38414622b0affdfe50a9fb7f1577af89d
Francesca Cella <francescacella@easy-network.it>
2021-03-14 12:52:12 +0100

    Add blue triangle

--- Francesca_Cella_65172_Esercizio_1.m
+++ Francesca_Cella_65172_Esercizio_1.m
@@ -16,14 +16,29 @@ quiver3(0,0,0,1,0,0,'Color','r','autoscale','off')
 quiver3(0,0,0,0,1,0,'Color','g','autoscale','off')
 quiver3(0,0,0,0,0,1,'Color','b','autoscale','off')
 % Definizione del punto di intersezione
-L = randi([MIN_LIM MAX_LIM], 1, 3);
+L = [7 8 9];
 plot3(L(1), L(2), L(3), '.r', 'MarkerSize', 12);
 text(L(1) + .3, L(2) + .3, L(3) + .3, 'L');
 % Definizione piano
-% 1. Definizione punto nello spazio per il quale passa la retta che vincola l' "inclinazione" del piano rispetto a Z
-line = randi([MIN_LIM MAX_LIM], 1, 3);
+% 1. Definizione punto nello spazio per il quale passa la retta
+% che vincola l' "inclinazione" del piano rispetto a Z
+line = [1 2 3];
 % 2. Ricavare la matrice Z: l*x+m*y+n*z=0 -> %z=-(l*x+m*y)/n
 [X, Y] = meshgrid(MIN_LIM:STEP:MAX_LIM, MIN_LIM:STEP:MAX_LIM);
-Z=-(line(1) * X + line(2) * Y)/line(3);
+Z = -(line(1) * X + line(2) * Y)/line(3);
 surf(X, Y, Z, 'FaceAlpha', STEP, 'EdgeColor', 'none');
+% Generazione triangolo blu
+% 1. Definizione punti
+A = [4 5 6];
+plot3(A(1), A(2), A(3), '.b', 'MarkerSize', 6);
+B = [5 6 4];
+plot3(B(1), B(2), B(3), '.b', 'MarkerSize', 6);
+C = [6 4 5];
+plot3(C(1), C(2), C(3), '.b', 'MarkerSize', 6);
+% 2. Definizione segmenti
+plot3(A, B, C, '-b', 'LineWidth', 1.5);
+plot3(C, A, B, '-b', 'LineWidth', 1.5);
+% Generazione triangolo rosso
+
+
 
